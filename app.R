5931384f3a47ad93d2daa315987a3352c8951776
LRF <linn.friberg@pm.me>
2024-11-19 18:03:55 +0100

    Bugfix

--- app.R
+++ app.R
@@ -1,12 +1,7 @@
 # A Shiny app that allows users to search for items on Tradera using a query.
 # The app retrieves search results and displays them in a data table format.
 
-# Install prerequisites in the following way:
-
-# install.packages("shiny")
-# install.packages("data.table")
-# install.packages("DT")
-# devtools::install_github("aurora-mm/TradeRa")
+devtools::install_github("aurora-mm/TradeRa")
 
 library(shiny)
 library(data.table)
@@ -36,39 +31,20 @@ server <- function(input, output, session) {
   observeEvent(input$searchBtn, {
     # Call the Search function with the specified AppId, AppKey, etc.
     searchResult <- Search(
-      AppId = 5201,
-      AppKey = "458a01fd-c949-4ff0-bef3-740181491793",
-      pageNumber = 1,
+      AppId = 5205,
+      AppKey = "180bdaf7-0455-45b5-8750-69ac9225ee6f",
       orderBy = "Relevance",
       query = input$query
     )
-    # Get all pages if total_pages is larger than 1
-    if (searchResult$total_pages > 1) {
-      final_df <- data.frame()
-      for (i in 1:searchResult$total_pages)
-      {
-        searchResult <- Search(
-          AppId = 5201,
-          AppKey = "458a01fd-c949-4ff0-bef3-740181491793",
-          pageNumber = i,
-          orderBy = "Relevance",
-          query = input$query
-        )
-        final_df <- rbind(final_df, searchResult$df)
-      }
-      searchResult$df <- final_df
-      searchResult$df <- searchResult$df[searchResult$df$ShortDescription != "Nothing found!", ]
-    }
-
-
+    
     # Extract the data frame from the search result
-    if (!is.null(searchResult$df)) {
+    if (!is.null(searchResult)) {
       # Select relevant columns for display
       results$df <- data.table(
-        ShortDescription = searchResult$df$ShortDescription,
-        LongDescription = searchResult$df$LongDescription,
-        Price = searchResult$df$Price,
-        ItemUrl = searchResult$df$ItemUrl
+        ShortDescription = searchResult$ShortDescription,
+        LongDescription = searchResult$LongDescription,
+        Price = searchResult$Price,
+        ItemUrl = searchResult$ItemUrl
       )
     } else {
       results$df <- data.table(
@@ -82,6 +58,7 @@ server <- function(input, output, session) {
 
   # Render the data table with the search results
   output$searchResults <- renderDataTable({
+    req(results$df)
     results$df
   })
 }
